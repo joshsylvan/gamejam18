e78d3da78f69ce5a956f004e624cfe89a510f2cc
Joshua Gabrel <joshgabrel@live.co.uk>
2018-01-27 21:12:16 +0000

    room stats and json variable set up

---
+++ Assets/RoomManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomManager : MonoBehaviour
+{
+	private GameObject[] rooms;
+	private GameObject map;
+	
+	// Use this for initialization
+	void Start () {
+		this.map = GameObject.FindWithTag("Map");
+		rooms = new GameObject[map.transform.childCount];
+
+		for (int i = 0; i < map.transform.childCount; i++)
+		{
+			this.rooms[i] = map.transform.GetChild(i).gameObject;
+			this.rooms[i].GetComponent<RoomStats>().Init();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public GameObject[] GetRooms()
+	{
+		return rooms;
+	}
+}
---
+++ Assets/RoomStats.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomStats : MonoBehaviour
+{
+	private bool trap1, trap2;
+	private string type;
+	public string trap1Name, trap2Name;
+	public int startX, startY, endX, endY;
+	private bool isPlayerHere;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Init()
+	{
+		trap1 = false;
+		trap2 = false;
+		this.type = this.gameObject.name;
+	}
+	
+	public void TriggerTrap1()
+	{
+		trap1 = true;
+		trap2 = false;
+	}
+
+	public void TriggerTrap2()
+	{
+		
+	}
+
+	public bool HasTrap1BeenUsed()
+	{
+		return trap1;
+	}
+	
+	public bool HasTrap2BeenUsed()
+	{
+		return trap2;
+	}
+
+	public string GetTrap1Name()
+	{
+		return this.trap1Name;
+	}
+	
+	public string GetTrap2Name()
+	{
+		return this.trap2Name;
+	}
+
+	public string GetRoomName()
+	{
+		return type;
+	}
+
+	public void SetIsPlayerHere(bool isPlayerHere)
+	{
+		this.isPlayerHere = isPlayerHere;
+	}
+	
+}
