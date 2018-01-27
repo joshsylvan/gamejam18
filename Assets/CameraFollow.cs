78a5706e3400b8e8643daf2bcd982f32a6052ff5
Joshua Gabrel <joshgabrel@live.co.uk>
2018-01-27 01:35:19 +0000

    camera movement with right stick

--- Assets/CameraFollow.cs
+++ Assets/CameraFollow.cs
@@ -19,7 +19,14 @@ public class CameraFollow : MonoBehaviour
 	{
 
 		Vector3 newPos = Vector3.Lerp(this.transform.position, this.target.transform.position, Time.deltaTime*this.cameraSpeed);
+
+		float xJoyStixDirection = XboxCtrlrInput.XCI.GetAxis(XboxCtrlrInput.XboxAxis.RightStickX);
+		float yJoyStixDirection = XboxCtrlrInput.XCI.GetAxis(XboxCtrlrInput.XboxAxis.RightStickY);
 		
+		Vector3 cameraOffset = new Vector3(xJoyStixDirection/20f, yJoyStixDirection/20f, -10);
+		newPos += cameraOffset;
+		
+		Debug.Log(xJoyStixDirection + " : " + yJoyStixDirection);
 		this.transform.position = new Vector3(newPos.x, newPos.y, -10);
 			
 	}
