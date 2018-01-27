994abe34853b4ed799e7b15669cc30c9211f9b85
Joshua Gabrel <joshgabrel@live.co.uk>
2018-01-27 01:14:41 +0000

    Camera Follow init

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+	private Transform target;
+	public float cameraSpeed = 5f;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		target = GameObject.FindWithTag("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+		Vector3 newPos = Vector3.Lerp(this.transform.position, this.target.transform.position, Time.deltaTime*this.cameraSpeed);
+		
+			
+
+	}
+	
+	
+	
+}
