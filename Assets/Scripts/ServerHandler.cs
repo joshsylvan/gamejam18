7deb0861356a894c88dc4db58492dae54efd33a3
Joshua Gabrel <joshgabrel@live.co.uk>
2018-01-27 00:41:53 +0000

    Sever pinging added

--- Assets/Scripts/ServerHandler.cs
+++ Assets/Scripts/ServerHandler.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using NUnit.Framework;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class ServerHandler : MonoBehaviour {
 
@@ -10,7 +11,9 @@ public class ServerHandler : MonoBehaviour {
 	
 	// Use this for initialization
 	public string url = "https://api.github.com/users/joshsylvan";
+	private string urlUpload = "https://api.github.com/users/joshsylvan";
 	private WWW response;
+	private ServerResponce sr;
 	
 	void Start () 
 	{
@@ -30,28 +33,39 @@ public class ServerHandler : MonoBehaviour {
 		if (www.error == null)
 		{
 			response = www;
-			Debug.Log( response);
-			Debug.Log( response.text);
-			Debug.Log( response.bytes);
-			string jsonString;
-			try
-			{
-				jsonString = System.Text.Encoding.UTF8.GetString(response.bytes, 3, response.bytes.Length - 3);
-			
+//			string jsonString;
+//			jsonString = System.Text.Encoding.UTF8.GetString(response.bytes, 3, response.bytes.Length - 3);
 
 //			ServerResponce json = JsonUtility.FromJson<ServerResponce>(jsonString);
-			ServerResponce sr = JsonUtility.FromJson<ServerResponce>(jsonString);
+			sr = JsonUtility.FromJson<ServerResponce>(response.text);
 			Debug.Log("WWW Ok!: " + sr.login);
-			}
-			catch (Exception e)
-			{
-			}
+			
 		} 
 		else
 		{
 			Debug.Log("WWW Error: "+ www.error);
 		}    
 	}
+	
+	IEnumerator UploadData(int health, float x, float y) {
+		WWWForm form = new WWWForm();
+		form.AddField("myField", "myData");
+
+		using (UnityWebRequest www = UnityWebRequest.Post("http://www.my-server.com/myform?health=" + health, form))
+		{
+			yield return www.Send();
+
+			if (www.isNetworkError || www.isHttpError)
+			{
+				Debug.Log(www.error);
+			}
+			else
+			{
+				Debug.Log("Form upload complete!");
+			}
+		}
+	}
+
 	
 	// Update is called once per frame
 	void Update () {
